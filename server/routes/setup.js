e4a299f110527b0f21cfa1a363b60c44e8620da8
eplamondon2 <eplamondon@hyundaistraymond.ca>
2026-05-15 20:39:36 -0400

    Update setup.js

--- server/routes/setup.js
+++ server/routes/setup.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
       INSERT INTO users (nom, prenom, email, password, role, initiales) VALUES
         ('Langevin',  'Nancy',    'nancy.langevin@hyundaistraymond.ca',  '$2a$12$rPvaJpwww.0V625oMrQiweuUs.cIfJ9zdjtqf8WBAmNuIBf5Yd3bS', 'directeur',  'NL'),
         ('Boulet',    'Francois', 'francois.boulet@hyundaistraymond.ca', '$2a$12$dfNtEJmwMLNycvi69G6geOn4xSmBtkRKGHlv3emuT1oRKWDRiqnx.', 'conseiller', 'FB'),
-        ('Plamondon', 'Etienne',  'eplamondon@hyundaistraymond.ca',       '$2a$12$yVr6R6QZaBtFk1TwnZf2MuPvkYYwXnHE64fTJTjnOFM7fRj7fZUMa', 'conseiller', 'EP'),
+       ('Plamondon', 'Etienne',  'eplamondon@hyundaistraymond.ca', '$2a$12$yVr6R6QZaBtFk1TwnZf2MuPvkYYwXnHE64fTJTjnOFM7fRj7fZUMa', 'admin', 'EP'),
         ('Dube',      'Johanne',  'jdube@hyundaistraymond.ca',           '$2a$12$/P0nqO9amn6N5O08BbPLZeAG2OkEb8HPWmZFAHuC.yPCPa934ERrS', 'preposee',   'JD'),
         ('Admin',     'Service',  'admin@hyundaistraymond.ca',           '$2a$12$r0vSTKNNabIarG89xUqjNuqckCH7HJpJqNSVBt.m0qsgU6klVBWpS', 'admin',      'AD')
       ON CONFLICT (email) DO NOTHING
